6c3b7158557a42e23e3b81e26dfc21387d1c91b6
Anastasia Alt <anastasia.alt@gmail.com>
2016-01-13 17:15:11 -0500

    initial commit

--- app.js
+++ app.js
@@ -7,9 +7,35 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
 
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var mongoUrl = "mongodb://localhost:27017/myDB";
+var db;
+
+MongoClient.connect(mongoUrl, function(err, database){
+  if (err) {
+    console.log(err);
+  }
+  console.log("connected!");
+  db = database;
+  process.on('exit', db.close);
+})
+
 // Routes
 app.get('/', function(req, res){
   res.render('index');
+});
+
+app.get('/sightings/new', function(req, res){
+  res.render('form');
+});
+
+app.get('/api/sightings', function(req, res){
+  res.render('form');
+});
+
+app.post('/sightings', function(req, res){
+  console.log("Submit form");
 });
 
 app.listen(process.env.PORT || 3000);
--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,21 @@ console.log("        <')_,/ ")
 console.log("        (_==/  ")
 console.log("birder  ='-    ")
 console.log("we're hiring!  ")
+
+$updateButton.on('click', function(e) {
+  // Grab values from current card and save to an object
+  var hiveUpdate = {};
+  
+  hiveUpdate.location = $rendered.find('.location').text();
+  hiveUpdate.notes = $rendered.find('.notes').text();
+  hiveUpdate.num_bees = $rendered.find('.num-bees').text();
+  // make update call
+  $.ajax({
+    method: "PUT",
+    url: "/hives/" + hive.id,
+    data: hiveUpdate
+  }).done(function(data) {
+    // let user know you've saved
+    alert("I DID IT")
+  });
+});
